d6cf9859082e7f8c19bd87586b622b75c899cc7f
Chris Searle <chris@chrissearle.org>
2012-04-12 09:39:15 +0200

    Remove on the go

--- plus.google.com.js
+++ plus.google.com.js
@@ -1,4 +1,6 @@
 // Remove Trending
-$('div[componentid="13"][cat="1"]').hide();
+$('div[componentid="13"]').hide();
 // Remove Play a Game
-$('div[componentid="10"][cat="4"]').hide();
+$('div[componentid="10"]').hide();
+// Remove On the Go
+$('div[componentid="11"]').hide();
