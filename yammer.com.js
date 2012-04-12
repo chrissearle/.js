e9042a395e1d8640aceccb89b89b08d7fe1136c0
Chris Searle <chris@chrissearle.org>
2012-04-12 09:03:51 +0200

    Remove an awful lot of junk from yammers right column

---
+++ yammer.com.js
@@ -0,0 +1,5 @@
+var style = document.createElement('style');
+style.appendChild(document.createTextNode(''));
+document.head.appendChild(style);
+style.sheet.insertRule('#sidebar :nth-child(8), #getting-started-wrapper, #network-type, #sidebar .yj-component, #sidebar-group-suggestions-container { display: none !important }', 0);
+
